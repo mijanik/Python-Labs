3f608b52ab8d2c8ac092d89f9f8665f474275530
Miłosz Janik <mijanik@student.agh.edu.pl>
2022-10-22 22:14:11 +0200

    modified calculator to handle complex

--- zad16.py
+++ zad16.py
@@ -1,24 +1,36 @@
+class Complex:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def __add__(self, other):
+        return Complex(self.real + other.real, self.imag + other.imag)
+
+    def __sub__(self, other):
+        return Complex(self.real - other.real, self.imag - other.imag)
+
+    def set(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def print(self):
+        print(str(self.real) + " + " + str(self.imag) + "i")
+
 class Calculator:
     def __init__(self):
-        self.a = 0.0
-        self.b = 0.0
-        self.result = 0.0
+        self.a = Complex(0, 0)
+        self.b = Complex(0, 0)
+        self.result = Complex(0, 0)
 
-    def read(self, a, b):
-        self.a = a
-        self.b = b
+    def read(self, a_real, a_imag, b_real, b_imag):
+        self.a.set(a_real, a_imag)
+        self.b.set(b_real, b_imag)
 
     def sum(self):
-        self.result =  self.a + self.b
+        self.result = self.a + self.b
 
     def sub(self):
         self.result = self.a - self.b
-
-    def mul(self):
-        self.result = self.a * self.b
-
-    def div(self):
-        self.result = self.a / self.b
 
     def interface(self):
         print("Hello!")
@@ -27,20 +39,18 @@ class Calculator:
             choice = input("Enter: 0 - exit\n1 - Calculate!\n")
             if choice == "0":
                 return
-            a, sign, b = input("Enter equation example: 1 + 1\n").split()
-            a = float(a)
-            b = float(b)
-            self.read(a, b)
+            a_real, a_imag, sign, b_real, b_imag = input("Enter equation, example: -1 +2i - 2 -2i\n").split(" ")
+            self.read(float(a_real), float(a_imag[:-1]), float(b_real), float(b_imag[:-1]))
             match sign:
                 case "+":
                     self.sum()
                 case "-":
                     self.sub()
-                case "*":
-                    self.mul()
-                case "/":
-                    self.div()
-            print("Result = " + str(self.result))
+                case other:
+                    print("Invalid operation!")
+
+            print("Result = ")
+            self.result.print()
 
         return
 
