69f261ccce2196b77f046c69edd7a0561a441459
Miłosz Janik <mijanik@student.agh.edu.pl>
2022-10-24 15:04:25 +0200

    Third labs

--- zad18.py
+++ zad18.py
@@ -1,15 +1,52 @@
 import csv
-def main():
-    with open('example.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row)
 
-    #with open('example.csv', 'w', newline='') as file:
-    #    writer = csv.writer(file)
-    #    writer.writerow(["Task ID", "Task title", "Task description"])
-    #    writer.writerow([1, "Do Homework", "Maths"])
-    #    writer.writerow([2, "Make a bubble sort", "its important to make it today"])
+
+def read_csv():
+    with open('example.csv') as file:
+        reader = csv.reader(file, delimiter=',')
+        for row in reader:
+            print(', '.join(row))
+
+
+def add_csv(title: str, description: str):
+    fields = [title, description]
+    with open('example.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(fields)
+
+
+def rem_csv(to_remove: str):
+    lines = list()
+    with open('example.csv', 'r') as readFile:
+        reader = csv.reader(readFile)
+        for row in reader:
+            lines.append(row)
+            for field in row:
+                if field == to_remove:
+                    lines.remove(row)
+
+    with open('example.csv', 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(lines)
+
+
+def main():
+    print("Database content:\n")
+    read_csv()
+    choice = input("Enter: \n1 - write row to CSV\n2 - delete from CSV\n")
+
+    match choice:
+        case "1":
+            title = input("Type Title:\n")
+            description = input("Type Description:\n")
+            add_csv(title, description)
+
+        case "2":
+            to_remove = input("Type Title or Description of Task to remove:\n")
+            rem_csv(to_remove)
+
+        case _:
+            print("Invalid operation")
 
 
 if __name__ == '__main__':
---
+++ zad19.py
@@ -0,0 +1,47 @@
+import random
+import numpy as np
+import threading
+import time
+import collections
+import functools
+import operator
+import matplotlib.pyplot as plt
+
+Calculated_Thread1 = 0
+Calculated_Thread2 = 0
+
+dict1 = {}
+dict2 = {}
+
+thread_1_done = 0
+thread_2_done = 0
+
+
+def count_elements(seq, mydict: dict, thread_id: int) -> dict:
+    for i in seq:
+        mydict[i] = mydict.get(i, 0) + 1
+
+    return mydict
+
+
+def main():
+    data = np.random.randint(10, size=1000000)
+    threading.Thread(target=count_elements, args=(list(data[:len(data)//2]), dict1, 1)).start() #first half
+    threading.Thread(target=count_elements, args=(list(data[len(data)//2:]), dict2, 2)).start() #second half
+
+    while threading.active_count() != 1:
+        pass
+
+    print(threading.active_count())
+    print(dict1)
+    print(dict2)
+
+    res = dict(functools.reduce(operator.add, map(collections.Counter, [dict1, dict2])))
+
+    print(res)
+    plt.bar(list(res.keys()), res.values(), color='g')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
